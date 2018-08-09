30f791a230766c5101e6052228cab11c14073298
Arjun K Prasad <arjun.blr1237@gmail.com>
2018-08-09 10:18:52 +0100

    Add files via upload

---
+++ Send/Send.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "1", Password = "1", Port = 5672 };
+            factory.VirtualHost = "TestByArjun";
+
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
